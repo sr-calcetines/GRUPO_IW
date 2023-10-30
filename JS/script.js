26bb80520f330feb34d5db06024adf195128d13c
[pepe] <[pgavilansanc@gmail.com]>
2023-10-30 19:16:10 +0100

    primera entrada

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+document.getElementById("login-button").addEventListener("click", function() {
+    var usuario = document.getElementById("usuario").value;
+    var contrasena = document.getElementById("contrasena").value;
+
+    // Aquí puedes agregar lógica para verificar el usuario y contraseña, y redirigir si es válido.
+});
